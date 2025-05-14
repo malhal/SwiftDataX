954a69280ff9623ccbb613aed8d9134d59e2afb7
Malcolm Hall <malhal@users.noreply.github.com>
2025-05-14 16:12:37 +0100

    Update QueryView.swift
    Had the idea to change QueryView to return a Result type

--- Sources/SwiftDataX/QueryView.swift
+++ Sources/SwiftDataX/QueryView.swift
@@ -3,14 +3,25 @@ import SwiftUI
 
 public struct QueryView<Model, Content>: View where Model: PersistentModel, Content: View {
     @Query var models: [Model]
-    let content: ([Model]) -> Content
     
-    public init(query: Query<Model, [Model]>, @ViewBuilder content: @escaping ([Model]) -> Content) {
+    public typealias QueryViewResult = Result<[Model], Error>
+    let content: (QueryViewResult) -> Content
+    
+    public init(query: Query<Model, [Model]>, @ViewBuilder content: @escaping (QueryViewResult) -> Content) {
         self.content = content
         self._models = query
     }
     
+    var result: QueryViewResult {
+        if let error = _models.fetchError {
+            return .failure(error)
+        }
+        else {
+            return .success(models)
+        }
+    }
+    
     public var body: some View {
-        content(models)
+        content(result)
     }
 }
